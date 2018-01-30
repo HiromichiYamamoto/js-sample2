56169d016fa8e00dd95b4f66a3dc9cce7588dda7
HiromichiYamamoto <fig@Fig.local>
2018-01-30 16:51:32 +0900

    add router

--- scripts/app5-2.js
+++ scripts/app5-2.js
@@ -1,49 +1,114 @@
 ;(function(){
 
   var $pages;
+  var pageObjects = [];
+  var urlHistory = []; //前・次のURL用の配列
+
+  function pageFactory(url, $el, enter, leave) {
+    return {
+      url: url,
+      $el: $el,
+      enter: enter || pageEnter,
+      leave: leave || pageLeave
+    }
+  }
+
+  function getPage(pages, key){
+    return pages.filter(function(e){
+      return e.url == key;
+    })[0] || null;
+  }
+
+  function pageEnter($el) {
+    var $page = $el.addClass('page-enter').appendTo('article');
+    return animEnd( $page ).then(function(){
+      $el.removeClass('page-enter');
+    });
+  }
+
+  function pageLeave($el) {
+    return animEnd( $el.addClass('page-leave') ).then(function() {
+      $el.detach();
+      $el.removeClass('page-leave');
+    });
+  }
+
+  function scanLast(arr, f){
+    var temp = arr.slice(-2);
+    if(temp.length === 1) temp.unshift(null);
+    return f.apply(this, temp);
+  }
+
+  var firstPromise = new $.Deferred().resolve();
 
   function urlChangeHandler() {
     var pageid = parseUrl( location.hash );
-    var $prevPage = $pages.filter(":visible");
-    var $nextPage = $pages.filter(".page"+pageid);
 
-    function enter() {
-      $pages.detach();
+    urlHistory.push( pageid );
 
-      $nextPage
-        .removeClass("page-enter")
-        .appendTo("article")
-        .addClass("page-enter");
-    }
+    scanLast(urlHistory, function(prev, next){
+      var prevPage = getPage(pageObjects, prev)
+        , nextPage = getPage(pageObjects, next);
 
-    if($prevPage.length > 0) {
-      $prevPage
-        .addClass("page-leave")
-        .on("webkitAnimationEnd", function onFadeOut(){
-          $prevPage.off("webkitAnimationEnd", onFadeOut);
-
-          $nextPage
-            .removeClass("page-leave")
-            .detach();
-          enter();
-        });
-    } else {
-      enter();
-    }
-
+      firstPromise.then(function(){
+        var page = prevPage;
+        if(page) return page.leave( page.$el, pageLeave.bind(this, page.$el), prev, next );
+      }).then(function() {
+        var page = nextPage;
+        return page.enter( page.$el, pageEnter.bind(this, page.$el), prev, next );
+      });
+    });
   };
 
-  function parseUrl(url) {
-    return url.slice(1) ||1;
+  function animEnd($el) {
+    var dfd = new $.Deferred
+      , callback = function(){ dfd.resolve($el); };
+
+    if( $el.length === 0 || $el.css("-webkit-animation") === undefined ) {
+      dfd.resolve();
+      return dfd;
+    }
+
+    $el.on( "webkitAnimationEnd", callback );
+    dfd.done(function() {
+      $el.off( "webkitAnimationEnd", callback );
+    });
+
+    return dfd;
   };
 
-  function init() {
-    $pages = $("[data-role='page']").detach();
+  function parseUrl(url) { return url.slice(1) ||1; };
+
+  function start() {
     $(window)
       .on("hashchange", urlChangeHandler)
       .trigger("hashchange");
+  }
+
+  function add(url, $el, enter, leave) {
+    pageObjects.push( pageFactory(url, $el, enter, leave) );
+  }
+
+
+  window.myRouter = {
+    add: add,
+    start: start
   };
 
-  init();
-
 })();
+
+myRouter.add( "1", $(".page1"), function($el, action, prev, next){
+  alert("hello");
+  return action();
+}, function($el, action, prev, next){
+  alert("bye");
+  return action();
+} );
+
+myRouter.add( "2", $(".page2"), null, null );
+myRouter.add( "3", $(".page3"), null, null );
+myRouter.add( "4", $(".page4"), null, null );
+
+$("[data-role='page']").detach();
+
+myRouter.start();
